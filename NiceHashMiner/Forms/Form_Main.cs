0c0c0c59f03854698792f1b42a14b2904b306050
Stanko Krstić <krsticch@gmail.com>
2016-12-12 17:44:33 +0100

    remove postfix

--- NiceHashMiner/Forms/Form_Main.cs
+++ NiceHashMiner/Forms/Form_Main.cs
@@ -50,7 +50,7 @@ namespace NiceHashMiner
         int flowLayoutPanelVisibleCount = 0;
         int flowLayoutPanelRatesIndex = 0;
                 
-        const string _betaAlphaPostfixString = "_NET20_backport";
+        const string _betaAlphaPostfixString = "";
 
         private bool _isDeviceDetectionInitialized = false;
 
