61f27ef65362bd23682d89e4b2b19e6237cd4b0a
Stanko Krstić <krsticch@gmail.com>
2017-03-24 13:47:42 +0100

    pre-rel ver change

--- NiceHashMiner/Forms/Form_Main.cs
+++ NiceHashMiner/Forms/Form_Main.cs
@@ -51,7 +51,7 @@ namespace NiceHashMiner
         int flowLayoutPanelVisibleCount = 0;
         int flowLayoutPanelRatesIndex = 0;
 
-        const string _betaAlphaPostfixString = "-Pre-Release_02";
+        const string _betaAlphaPostfixString = "-Pre-Release_03";
 
         private bool _isDeviceDetectionInitialized = false;
 
